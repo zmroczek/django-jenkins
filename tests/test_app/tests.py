a6061063361f4e3b9ba18d155d5bae093b369f9b
Mikhail Podgurskiy <kmmbvnr@gmail.com>
2012-05-04 12:47:03 +0700

    New unittest2 compatible test runner

--- tests/test_app/tests.py
+++ tests/test_app/tests.py
@@ -4,6 +4,7 @@ from django.test import TestCase
 
 class SaintyChecks(TestCase):
     def test_mailbox_stubs_not_broken(self):
+        print "Testing mailbox django stubs"
         mail.send_mail('Test subject', 'Test message', 'nobody@kenkins.com',
                        ['somewhere@nowhere.com'])
         self.assertTrue(1, len(mail.outbox))
